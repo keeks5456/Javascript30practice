b01d1b7e4d8a156e5e39b77ee3ff8f8c67abe205
keeks5456 <Akikogreen08@gmail.com>
2020-12-09 17:20:51 -0800

    started on css variables & js

--- JSclock/clock.js
+++ JSclock/clock.js
@@ -1,9 +1,27 @@
+const minuteHand = document.querySelector('.min-hand')
+const hourHand = document.querySelector('.hour-hand')
 const secondHand = document.querySelector('.second-hand')
-// console.log(secondHand)
+// debugger
+
 function setDate(){
 const now = new Date()
-const seconds = now.getSeconds()
-const secondsDegrees = ((seconds / 60) * 360)
-secondHand.style.transform = `rotate(${secondsDegrees} deg)`
+
+const seconds = now.getSeconds() //JS function
+// how can we turn this to a degree?
+const secondsDegrees = ((seconds / 60) * 360) + 90 
+secondHand.style.transform = `rotate(${secondsDegrees}deg)`
+
+const minutes = now.getMinutes()
+const minutesDegrees = ((minutes / 60) * 360) + ((seconds/60)* 6) + 90
+minuteHand.style.transform = `rotate(${minutesDegrees}deg)`
+
+const hours = now.getHours()
+const hourDegrees = ((hours / 12) * 360) + ((minutes/60)*30) + 90
+minuteHand.style.transform = `rotate(${hourDegrees}deg)`
 }
+debugger
+setInterval(setDate, 1000);
+
+setDate();
+
 
